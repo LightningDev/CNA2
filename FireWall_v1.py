7d87dcd48e9a83c113953d391c9ed99ffc408284
Nhat Tran <lightningdev.freelancer@gmail.com>
2014-10-15 04:30:46 +1000

    Testing

--- FireWall_v1.py
+++ FireWall_v1.py
@@ -12,7 +12,7 @@ class MyFireWall(object):
 
     def __init__(self, connection):
         self.connection = connection
-        connection.addListenter(self)
+        connection.addListenters(self)
         self.table = {}
         self.firewall = {}
         self.set_rule(0x800, 1, 0, of.OFPP_ALL)
